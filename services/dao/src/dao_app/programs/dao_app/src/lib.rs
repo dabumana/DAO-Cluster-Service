8b5754b6f05027154e145bbebef2bc554b44f0c2
dabumana <dabumana@gmail.com>
2021-12-23 20:37:01 -0500

    PoC Update
    Change Log:
    * Node scaffold added for FE.

--- services/dao/src/dao_app/programs/dao_app/src/lib.rs
+++ services/dao/src/dao_app/programs/dao_app/src/lib.rs
@@ -1,14 +1,43 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("92WfyRc8mUxRNJTk9ajSPLQ6MwFyNaexJgHmQv82wbi8");
 
 #[program]
-pub mod dao_app {
+mod dao_app {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn create(ctx: Context<Create>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
         Ok(())
     }
 }
 
+// Transaction instructions
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Transaction instructions
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// An account that goes inside a transaction instruction
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
--- services/dao/src/dao_app/tests/dao_app.ts
+++ services/dao/src/dao_app/tests/dao_app.ts
@@ -2,16 +2,45 @@ import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import { DaoApp } from '../target/types/dao_app';
 
-describe('dao_app', () => {
+const assert = require("assert");
+const { SystemProgram } = anchor.web3;
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+describe("dao_app", () => {
+  /* create and set a Provider */
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.dao_app;
+  it("Creates a counter)", async () => {
+    /* Call the create function via RPC */
+    const baseAccount = anchor.web3.Keypair.generate();
+    await program.rpc.create({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
 
-  const program = anchor.workspace.DaoApp as Program<DaoApp>;
+    /* Fetch the account and check the value of count */
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Count 0: ', account.count.toString())
+    assert.ok(account.count.toString() == 0);
+    _baseAccount = baseAccount;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  });
+
+  it("Increments the counter", async () => {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.increment({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Count 1: ', account.count.toString())
+    assert.ok(account.count.toString() == 1);
   });
 });
